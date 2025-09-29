cb4a334bf18e2db64d37876f1275f16210ac080f
komuw <komuw05@gmail.com>
2025-09-29 10:00:02 +0300

    my_wpf_app: target .net v4.7.2 We have hardcoded c# version to 10 since there are some features we are depending on. However, we should eventually target the corresponding c# version(7.3) for .net 4.7
    Signed-off-by: komuw <komuw05@gmail.com>

--- my_wpf_app/MainWindow.xaml.cs
+++ my_wpf_app/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,32 +13,32 @@ using System.Globalization;
 using System.Threading;
 using System.IO;
 
-namespace my_wpf_app;
+namespace my_wpf_app {
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window {
-  // This is the code-behind.
-  public MainWindow() {
-    InitializeComponent();
-  }
-  private void MyBtnId(object sender, RoutedEventArgs e) {
-    myTxtBlk.Text = "Awe btn clicked";
-    // e.Handled = true;  // Stops event bubbling.
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    // This is the code-behind.
+    public MainWindow() {
+      InitializeComponent();
+    }
+    private void MyBtnId(object sender, RoutedEventArgs e) {
+      myTxtBlk.Text = "Awe btn clicked";
+      // e.Handled = true;  // Stops event bubbling.
 
-    var msg = $@"MyBtnId was clicked
+      var msg = $@"MyBtnId was clicked
       sender={sender}
       event={e}\n";
-    File.AppendAllText("event_log.txt", msg);
-  }
+      File.AppendAllText("event_log.txt", msg);
+    }
 
-  [STAThread]
-  public static void Main() {
-    Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr");
-    var app = new Application();
-    var window = new MainWindow();
-    app.Run(window);
+    [STAThread]
+    public static void Main() {
+      Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr");
+      var app = new Application();
+      var window = new MainWindow();
+      app.Run(window);
+    }
   }
 }
-
