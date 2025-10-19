6a46a5fbca00c61da9b74e4798e774c62e7d31b6
komuw <komuw05@gmail.com>
2025-10-19 22:53:38 +0300

    cpp_stuff: add ability to compile multiple files
    Signed-off-by: komuw <komuw05@gmail.com>

--- cpp_stuff/app_one/main.cpp
+++ cpp_stuff/app_one/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 /* run;
-msbuild app_one.vcxproj -p:Platform=x64 -p:Configuration=Debug -warnAsError
-./x64/Debug/app_one.exe
+windows:
+  msbuild app_one.vcxproj -p:Platform=x64 -p:Configuration=Debug -warnAsError
+  ./x64/Debug/app_one.exe
+
+linux:
+  cd build && cmake .. && make && cd ..
+  ./build/app_one
 */
 
+int add(int x, int y);
+
 int main() {
-  std::cout << "Hello";
+  std::cout << add(2, 3);
   return 0;
 }
