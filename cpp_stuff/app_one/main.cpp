8befd8655fbc45db42f1e66f44ac54e974895bde
komuw <komuw05@gmail.com>
2025-10-20 11:29:03 +0300

    cpp_stuff:: add notes on creating and using dll in windows
    Signed-off-by: komuw <komuw05@gmail.com>

--- cpp_stuff/app_one/main.cpp
+++ cpp_stuff/app_one/main.cpp
@@ -8,7 +8,14 @@ windows:
 linux:
   cd build && cmake .. && make && cd .. && ./build/app_one
     or
-  g++ -std=c++14 -Wall main.cpp add.cpp -o build/app_one && ./build/app_one
+  g++ -std=c++14 -g3 -O0 -Wall main.cpp add.cpp -o ./build/app_one && ./build/app_one
+
+to create dll:
+  # https://nullprogram.com/blog/2021/05/31/
+  # Use `w64devkit` terminal to run the commands.
+  g++ -std=c++14 -g3 -O0 -Wall -shared -s add.cpp -o add.dll # you don't need to specify `add.h`
+  g++ -std=c++14 -g3 -O0 -Wall -s main.cpp add.dll -o app_one.exe # use the dll.
+  peports -i -e add.dll # to inspect the dll exports/imports. Also see `dumpbin /EXPORTS /IMPORTS /DEPENDENTS add.dll` in vs2019 dev cmd.
 */
 
 
